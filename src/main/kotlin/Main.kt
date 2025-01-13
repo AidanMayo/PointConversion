b1a90aeecbbc1f8d3c1c68c1ab5ff544474133d2
aidanmao2005 <aidanmao2005@gmail.com>
2025-01-13 15:05:13 -0500

    distances, fov checks, rotations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 package org.example
 
+import kotlin.math.atan2
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 fun main() {
     /*
     * Point math is easily rotated by understanding how sine waves work
@@ -10,28 +14,65 @@ fun main() {
     * This is the fundamental component that allows us to convert 3D points with a given rotation to a 2d picture from a camera.
      */
 
-    val point = point(4.65, 23.34, 23.45)
 
 
+    println(atan2(0.0, 1.0)) // pi/4
+}
+
+fun degreeToRad(degree: Double): Double {
+    return degree * (Math.PI/180)
 }
 
 class screen() {
-    val points = arrayListOf<point>()
+    val points = arrayListOf<threeDimPoint>()
 
     val camera = camera()
 
     // I want to create a camera that can see within a given FOV
+
+    fun snapshot(): ArrayList<twoDimPoint> {
+        val displayedPoints = arrayListOf<twoDimPoint>()
+
+        for (point in points) {
+            if (camera.viewDistance < camera.cameraPosition.calculateDistance(point)) {
+                continue
+            }
+
+            // In FOV Check
+
+
+            // Place point in 2D
+            displayedPoints.add(twoDimPoint(0.0, 0.0) /* Write code to change to 2d spot */)
+        }
+
+        return displayedPoints
+    }
 }
 
 // Camera starts
 class camera(
-    var viewDistance: Double = 1.0,
-    var FOV: Double = 90.0,
-    var cameraPosition: point = point(1.0, 0.0, 0.0),
-    var xRot: Double = 0.0,
-    var yRot: Double = 0.0,
-    var zRot: Double = 0.0
+    var viewDistance: Double = 1.0, // View Distance of camera | Works as a radius from the camera
+    var cameraPosition: threeDimPoint = threeDimPoint(1.0, 0.0, 0.0), // Camera Position
+    var xRot: Double = 0.0, // Camera X Rotation (Left-Right)
+    var yRot: Double = 0.0, // Camera Y Rotation (Up-Down)
+    var zRot: Double = 0.0 // Camera Z Rotation (Clock-style)
 ) {
+    // FOV Must be between 30.0 and 160.0
+    var FOV: Double = 90.0
+        set(value) { field = value.coerceIn(30.0, 160.0) }
+
+    // Increase FOV by a given amount
+    // Caps at 160 FOV
+    fun increaseFOV(FOVIncrease: Double) {
+        FOV += FOVIncrease
+    }
+
+    // Decrease FOV by a given amount
+    // Caps at 30 FOV
+    fun decreaseFOV(FOVDecrease: Double) {
+        FOV -= FOVDecrease
+    }
+
     // Moves camera along axis
     fun moveCam(x: Double, y: Double, z: Double) {
         this.cameraPosition.transform(x, y, z)
@@ -42,6 +83,12 @@ class camera(
     // Positive vertical moves up
     fun encircle(horiRot: Double, vertRot: Double) {
         this.rotate(-horiRot, -vertRot, 0.0)
+
+        // horizontal
+        val horRad = sqrt(cameraPosition.X.pow(2) + cameraPosition.Z.pow(2))
+        val horRot = atan2(cameraPosition.X, cameraPosition.Z)
+
+
     }
 
     // ! Rotation should automatically truncate to a range of -180 to 180 !
@@ -68,7 +115,21 @@ class camera(
     }
 }
 
-class point(
+class twoDimPoint(
+    var x: Double,
+    var y: Double
+) {
+    fun transform(x: Double, y: Double) {
+        this.x += x
+        this.y += y
+    }
+
+    fun calculateDistance(other: twoDimPoint) : Double {
+        return sqrt((other.x - this.x).pow(2) + (other.y - this.y).pow(2))
+    }
+}
+
+class threeDimPoint(
     var X: Double,
     var Y: Double,
     var Z: Double
@@ -81,5 +142,9 @@ class point(
         this.X += X
         this.Y += Y
         this.Z += Z
+    }
+
+    fun calculateDistance(other: threeDimPoint) : Double {
+        return sqrt((other.X - this.X).pow(2.0) + (other.Y - this.Y).pow(2.0) + (other.Z - this.Z).pow(2.0))
     }
 }
